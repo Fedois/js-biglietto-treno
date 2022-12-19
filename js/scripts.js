e4143c938206120ae70812f0abc1f42ca4017473
Fedois <fedefox14@gmail.com>
2022-12-19 17:41:59 +0100

    correzione dettagli

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
 alert('clicca su "ok" per il fare il calcolo prezzo biglietto del treno')
 
-const kilometri = prompt('quanti kilometri vuoi percorrere?');
+let kilometri = parseInt(prompt('quanti kilometri vuoi percorrere?'));
 console.log('km: ', kilometri)
 
 const età = prompt('quanti anni hai?');
 console.log('età: ', età);
 
-let prezzokm = (kilometri * 0.21) .toFixed(2);
+let prezzokm = (kilometri * 0.21).toFixed(2);
 console.log('prezzo ticket: ', prezzokm + '€');
 
 if(età < 18){
     alert('avrai uno sconto del 20%');
     
-    let calcUno = ((prezzokm / 100) * 20) .toFixed(2);
+    let calcUno = ((prezzokm / 100) * 20).toFixed(2);
     console.log('sconto 20%: ', calcUno + '€');
 
     let totaleUno = (prezzokm - calcUno) .toFixed(2);
@@ -24,10 +24,10 @@ if(età < 18){
 else if(età >= 65){
     alert ('avrai uno sconto del 40%');
     
-    let calcDue = ((prezzokm / 100) * 40) .toFixed(2);
+    let calcDue = ((prezzokm / 100) * 40).toFixed(2);
     console.log('sconto 40%: ', calcDue + '€'); 
 
-    let totaleDue = (prezzokm - calcDue) .toFixed(2);
+    let totaleDue = (prezzokm - calcDue).toFixed(2);
     console.log('prezzo finale: ', totaleDue + '€')
 
     document.getElementById('prezzo').innerHTML = totaleDue + '€'; 
